4d83ec931972f2eec79c607cfb869ccc2540f3fa
Douglas <dougmarques10@gmail.com>
2016-03-09 09:06:11 -0300

    ActionMailer e Mailcatcher Cap. 08

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :users
   end
 
+  resource :confirmation, only: [:show]
+  
   get "/:locale" => "home#index", locale: LOCALES
   root "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
